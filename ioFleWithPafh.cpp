59480073a91264f9109b0bc5ba38f7c6ea73869b
galang <ahmad.galang.pamungkas24@gmail.com>
2025-06-10 18:20:01 +0700

    loop akan berhenti jika anda memasukan karakter q

--- ioFleWithPafh.cpp
+++ ioFleWithPafh.cpp
@@ -18,5 +18,7 @@ int main(){
         cout << "- ";
         //membuat setiap karakter dalam satu baris
         getline(cin, baris);
+        //loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q")break;
     }
 }
