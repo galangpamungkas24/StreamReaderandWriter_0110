e67f5c7fc7cc0b27b7d7b7140bc37a7e163b158c
galang <ahmad.galang.pamungkas24@gmail.com>
2025-06-10 17:32:10 +0700

    /menunjukan ke sebuah nama file

---
+++ ioFile.cpp
@@ -0,0 +1,16 @@
+
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int mian (){
+        string baris;
+        //membuat file dalam mode menulis
+        ofstream outfile;
+        //menunjukan ke sebuah nama file
+        outfile.open("contohfile.txt");
+        cout << ">= Meulis file, \'q\' untuk keluar" << endl;
+        //unlimited loop untuk menulis
+        
+}
