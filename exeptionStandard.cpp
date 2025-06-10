4adf1636889fac3af77f6d199ee4eaefb7f72d8c
galang <ahmad.galang.pamungkas24@gmail.com>
2025-06-10 18:37:01 +0700

    /penandaan 1 }

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -4,3 +4,8 @@
 //untuk obyek exception yg akan digunkan 
 #include <array>
 //untuk obyek array yg akan kita gukan
+using namespace std;
+
+int main(){
+    cout << "Awal Perogram " << endl;//penandaan 1
+}
