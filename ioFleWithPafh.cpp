fab1c809728693978e33712531a72a394a35f245
galang <ahmad.galang.pamungkas24@gmail.com>
2025-06-10 18:16:28 +0700

    // menunjukan ke sebuah nama file

--- ioFleWithPafh.cpp
+++ ioFleWithPafh.cpp
@@ -10,5 +10,8 @@ int main(){
     cin >> NamaFile;
     //membuat file dalam mode menulis.
     ofstream outfile;
-    
+    // menunjukan ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
 }
